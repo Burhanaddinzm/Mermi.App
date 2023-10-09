3721279d11380d32f3e415fb22f1f5c9af2d9109
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-10-10 00:48:23 +0400

    Optimized

--- Mermi App/Program.cs
+++ Mermi App/Program.cs
@@ -1,22 +1,13 @@
 ﻿//30 mermi var 1 e basanda ulduz cixir ve 1 mermi azalir 2 e basanda butun mermileri atir
 //3 e basanda reload edir 4 e basanda qalan mermileri gostersin 0 a basanda app dayansin
-
-int StopApp = 1;
 int Mermi = 30;
-while (StopApp > 0)
+while (true)
 {
     string App = Console.ReadLine();
-    if (App == "1")
+    if (App == "1" && Mermi > 0)
     {
-        if (Mermi > 0)
-        {
         Console.WriteLine("*");
         Mermi--;
-        }
-        else
-        {
-            Mermi = 0;
-        }
     }
     else if (App == "2")
     {
@@ -36,6 +27,6 @@ while (StopApp > 0)
     }
     else if (App == "0")
     {
-        StopApp = 0;
+        break;
     }
 }
